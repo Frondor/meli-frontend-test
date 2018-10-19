2f3faee67b8d365aaa38778ce9952ef13f4afb07
Frondor <frondor.dev@gmail.com>
2018-10-19 00:26:47 -0300

    config(sass): add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./Appi.sass";
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p className="classy">
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
